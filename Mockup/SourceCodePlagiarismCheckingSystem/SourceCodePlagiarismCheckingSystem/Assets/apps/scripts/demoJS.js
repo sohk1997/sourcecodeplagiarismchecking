2da669b788ac3a01fd4c9f3a5f692a44c87fa773
DuongGiaHoa <dghoa0603@gmail.com>
2019-05-19 21:59:04 +0700

    upfile+ editor -> nút submit -> modal config -> Compare 2 Editor

---
+++ Mockup/SourceCodePlagiarismCheckingSystem/SourceCodePlagiarismCheckingSystem/Assets/apps/scripts/demoJS.js
@@ -0,0 +1,46 @@
+﻿////Load file to code mirror
+var myTextArea = document.getElementById('code_editor');
+
+var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
+    lineNumbers: true,
+    theme: 'icecoder',
+    mode: 'javascript',
+    readoreadOnly: true
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('input-file')
+        .addEventListener('change', getFile);
+
+});
+
+
+function getFile(event) {
+    const input = event.target
+    if ('files' in input && input.files.length > 0) {
+        placeFileContent(
+            document.getElementById('code_editor'),
+            input.files[0])
+    }
+}
+
+function placeFileContent(target, file) {
+    readFileContent(file).then(content => {
+        target.value = content;
+        myCodeMirror.setValue(content);
+    }).catch(error => console.log(error))
+}
+
+function readFileContent(file) {
+    const reader = new FileReader()
+    return new Promise((resolve, reject) => {
+        reader.onload = event => resolve(event.target.result)
+        reader.onerror = error => reject(error)
+        reader.readAsText(file)
+
+    })
+}
+//--------------------------------------------------------------------
+
---
+++ Mockup/SourceCodePlagiarismCheckingSystem/SourceCodePlagiarismCheckingSystem/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SourceCodePlagiarismCheckingSystem.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CompareCode()
+        {
+            return View();
+        }
+
+    }
+}
