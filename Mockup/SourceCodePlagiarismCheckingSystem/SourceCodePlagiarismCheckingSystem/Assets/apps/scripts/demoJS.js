3146247f607eb5bf33f4b9f26a4fade47b6a3c7c
DuongGiaHoa <dghoa0603@gmail.com>
2019-05-23 10:44:51 +0700

    fix duplicate code view editor

--- Mockup/SourceCodePlagiarismCheckingSystem/SourceCodePlagiarismCheckingSystem/Assets/apps/scripts/demoJS.js
+++ Mockup/SourceCodePlagiarismCheckingSystem/SourceCodePlagiarismCheckingSystem/Assets/apps/scripts/demoJS.js
@@ -1,5 +1,4 @@
-﻿
-////Load file to code mirror
+﻿////Load file to code mirror
 var myTextArea = document.getElementById('code_editor');
 
 var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
@@ -9,21 +8,18 @@ var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
     readoreadOnly: true
 });
 
-
-
 document.addEventListener('DOMContentLoaded', function (event) {
     document.getElementById('input-file')
         .addEventListener('change', getFile);
 
 });
 
-
 function getFile(event) {
-    const input = event.target
+    const input = event.target;
     if ('files' in input && input.files.length > 0) {
         placeFileContent(
             document.getElementById('code_editor'),
-            input.files[0])
+            input.files[0]);
     }
 }
 
@@ -31,17 +27,17 @@ function placeFileContent(target, file) {
     readFileContent(file).then(content => {
         target.value = content;
         myCodeMirror.setValue(content);
-    }).catch(error => console.log(error))
+    }).catch(error => console.log(error));
 }
 
 function readFileContent(file) {
-    const reader = new FileReader()
+    const reader = new FileReader();
     return new Promise((resolve, reject) => {
-        reader.onload = event => resolve(event.target.result)
-        reader.onerror = error => reject(error)
-        reader.readAsText(file)
+        reader.onload = event => resolve(event.target.result);
+        reader.onerror = error => reject(error);
+        reader.readAsText(file);
 
-    })
+    });
 }
 //--------------------------------------------------------------------
 
