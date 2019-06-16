068aa010cf960cc081fb87a1f991015eb6cc4239
DuongGiaHoa <dghoa0603@gmail.com>
2019-06-16 13:30:57 +0700

    add template to web client

---
+++ Backend/WebClient/Controllers/UploadController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CompareCode()
+        {
+            return View();
+        }
+    }
+}
