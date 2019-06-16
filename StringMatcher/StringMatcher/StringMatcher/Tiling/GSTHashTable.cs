a0fddc4b84568f2e0f809961063d0b10bb33af79
Truong Le Gia Luat <sohk1997@gmail.com>
2019-06-16 09:25:02 +0700

    Add string matcher

---
+++ StringMatcher/StringMatcher/StringMatcher/Tiling/GSTHashTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringMatcher.Tiling
+{
+    public class GSTHashTable
+    {
+        private Dictionary<long, ICollection<int>> dict;
+
+        public GSTHashTable()
+        {
+            dict = new Dictionary<long, ICollection<int>>();
+        }
+
+        public void Add(long h, int obj)
+        {
+            ICollection<int> newlist = null;
+            if(dict.ContainsKey(h))
+            {
+                newlist = dict[h];
+                newlist.Add(obj);
+                dict.Add(h, newlist);
+            }
+            else
+            {
+                newlist = new List<int>();
+                newlist.Add(obj);
+                dict.Add(h, newlist);
+            }
+        }
+
+        public List<int> Get(long key)
+        {
+            if (dict.ContainsKey(key))
+                return dict[key].ToList();
+            else
+                return null;
+        }
+
+        public void Clear()
+        {
+            dict = new Dictionary<long, ICollection<int>>();
+        }
+    }
+}
---
+++ StringMatcher/StringMatcher/StringMatcher/Tiling/StringLengths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringMatcher.Tiling
+{
+    public class StringLengths
+    {
+        public readonly int id1StringLength;
+        public readonly int id2StringLength;
+        public StringLengths(int id1StringLength, int id2StringLength)
+        {
+            this.id1StringLength = id1StringLength;
+            this.id2StringLength = id2StringLength;
+        }
+    }
+}
