346622e24c8d33ffbb8956653e7e4e187b170541
DuongGiaHoa <dghoa0603@gmail.com>
2019-06-26 15:00:27 +0700

    web check abstract

---
+++ Backend/WebCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebCheck
+{
+    public abstract class Program
+    {
+        public Object Check(Guid id, string url)
+        {
+            return null;
+        }
+
+        public static string GETUrlAPI(string url)
+        {
+            return null;
+        }
+    }
+}
