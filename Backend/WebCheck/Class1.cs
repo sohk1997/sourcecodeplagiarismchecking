6d98109e8d563014f36a466403efa5739f844298
DuongGiaHoa <dghoa0603@gmail.com>
2019-07-01 17:33:20 +0700

    Upload and savefile to db

---
+++ Backend/WebCheck/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCheck
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            // Display the number of command line arguments:
+            Console.WriteLine();
+        }
+    }
+}
--- Backend/WebCheck/WebCheck.cs
+++ Backend/WebCheck/WebCheck.cs
@@ -4,9 +4,10 @@ using System.Text;
 
 namespace WebCheck
 {
+    
     public interface WebCheck
     {
         Result Check(Guid id, string url);
-
+       
     }
 }
